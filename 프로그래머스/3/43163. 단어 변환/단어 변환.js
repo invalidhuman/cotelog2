d09b6453fb21b66b4d8a00efbcba0e175734d831
seunggonhyeon <69204241+invalidhuman@users.noreply.github.com>
2026-09-04 16:03:30 +0900

    [level 3] Title: 단어 변환, Time: 0.21 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,18 +1,7 @@
 function solution(begin, target, words) {
     var answer = 0;
     
-    // begin, target
-    
-    // 최단거리? 경우의 수?
-    
-    // 근데 가장짧은 변환 과정을 찾는 거고 그 때마다 값을늘려가면서 해야하니 BFS인듯
-    
-    // words 안에 있는 단어로만 바꿀 수 있다. 겨룩ㄱ 단어 set까지 다 주어진 셈
-    
-    // hit -> hot -> dot -> dog -> cog 
-    // 그니까 words 안에는 무조건 target이 있다.
-    
-    // 하지만 words 안에 target이 없는 경우는 바로 return 0
+    // words 안에 target이 없는 경우는 바로 return 0
     if (!words.includes(target)) return 0
     
     
@@ -55,15 +44,9 @@ function solution(begin, target, words) {
             break
         }
         
-        // 그냥 어쩔 수 없이 하나씩 돌면서 하나 차이인지 아닌지를 봐야하는건가?
-        
-        // 그걸 그럼 queue에 넣어야하는이유가뭐지?
-        // queue에 넣는 이유는 단순히 그냥 다음으로 진행하기 위함?
-        // 아니면 일단 반복문을 돌면서 "현재"기준 하나 차이 나는 단어들을 경우의 수처럼 집어넣기 위해서. 
-        // 그럼 각 경우의 수에 대해 몇 단계가지 간 건지 같이 저자앻야함
+        // 그럼 각 경우의 수에 대해 몇 단계가지 간 건지 같이 저장
         for (let i = 0; i < words.length; i++) {
             // 알파벳 하나만 차이나면서 visited false인거 찾기
-            
             
             if (!visited[i] && isDiffOneLetter(word,words[i])) {
                 queue.push([words[i],step+1])
@@ -72,13 +55,6 @@ function solution(begin, target, words) {
         }
         
     }
-
-    // 어떻게하면 알파벳 하나 차이라는 걸 감지할 수 있을까?
-    // 반복문으로 누적해서 해야하나? 일단 같은 단어는 무조건 아니니까,
-    // flag 변수를 써서
-    // 1) 하나라도 다른게나오면 true로 했다가
-    // 2) 하나더 나오면 바로 false로 바꾸고 return
-    // 3) 끝까지 탐색후 return
     
     
     return answer;
