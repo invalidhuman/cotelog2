86c0226883f72482bb9d26f78b3b91701845f33d
seunggonhyeon <69204241+invalidhuman@users.noreply.github.com>
2026-09-05 23:56:17 +0900

    [level 1] Title: 모의고사, Time: 1.05 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -9,28 +9,28 @@ function solution(answers) {
     let countB = 0
     let countC = 0
     
-    // 각학생별 패턴을 바탕으로, 반복시켜서 answers와 매칭
+    // 각 학생별 패턴을 바탕으로, 반복시켜서 answers와 매칭
     for (let i =0; i< answers.length; i++) {
-        if (answers[i]===stuA[i % stuA.length]) countA++;
+        if (answers[i]===stuA[i % stuA.length]) countA++; // 항상 반복되니 나머지로 접근
         if (answers[i]===stuB[i % stuB.length]) countB++;
         if (answers[i]===stuC[i % stuC.length]) countC++;
     }
     
     const records = [countA, countB, countC]
     // console.log('records : ',records) // records : [ 5, 0, 0 ]
+    
     let result = []
     
     const highest = Math.max(...records) // js에서는 그냥 records를 넣지못함
     // console.log('highest : ',highest) // highest : 2
     
     // 같은 경우가 있을 수 있으니 highest와 비교해서 최고점수와 같은 학생들의 인덱스를 모두 배열에 push
+    // 하는 김에 for문으로 인덱스를 처음부터 접근하여 동점자를 오름차순 정렬을 할 필요없게 만듦
     for (let i = 0; i< 3; i++) {
-        if (records[i] === highest) result.push(i+1)
+        if (records[i] === highest) {
+            result.push(i+1)
+        }
     }
-    // console.log(result) // [ 1, 2, 3 ]
-    
-    // 해당 배열을 오름차순 정렬
-    result = result.sort((a,b)=>a-b); 
     
     return result
 }
